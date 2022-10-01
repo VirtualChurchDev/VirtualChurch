fa14a4f76a34078d75c779e880861c36689368ce
Ernests <ernests.kazakevics@gmail.com>
2022-10-01 10:22:01 +0300

    settings.py

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import mimetypes
+import django_heroku
 
 mimetypes.add_type("text/css", ".css", True)
 
@@ -112,6 +113,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
