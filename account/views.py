976f6c56e865fe1c286f1558f3cd4a1c4a2a71f8
Ernests <ernests.kazakevics@gmail.com>
2022-10-22 16:34:07 +0300

    email confirmation

--- account/views.py
+++ account/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from .forms import RegisterUserForm
-from .models import HeadUserAccess
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+
 from chat.models import Room
 from chat.views import pleaseLogin
+
+from .forms import RegisterUserForm
+from .models import HeadUserAccess
+from .tokens import account_activation_token
 
 def login_user(request):
     if request.method == 'POST':
@@ -24,16 +32,59 @@ def logout_user(request):
     logout(request)
     return redirect('/')
 
+def activateEmail(request, user, to_email):
+    mail_subject= "Aktivizē savu lietotāja kontu."
+    message = render_to_string('account/activate.html', {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        return True
+    else:
+        return False
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user=None
+    
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        
+        return render(request, 'message.html', {
+            'title': 'Konts tika aktivizēts!',
+            'text': 'Tagad Jūs varat pieslēgties ar jauno kontu.',
+        })
+    
+    return render(request, 'message.html', {
+        'title': 'Aktivizēšanas saite ir nederīga!',
+        'text': 'Izveido jaunu kontu vai sazinies ar palīdzības komandu.',
+    })
+
 def register_user(request):
     if request.method == 'POST':
         form = RegisterUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('/')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            if activateEmail(request, user, form.cleaned_data.get('email')):
+                return render(request, 'message.html', {
+                    'title': 'Konts ir gandrīz izveidots!',
+                    'text': 'Lai pabeigtu konta izveidi, Jums jādodas uz aktivizēšanas saiti, kas Jums tikko tika nosūtīta uz e-pastu ' + user.email + '!',
+                })
+            else:
+                return render(request, 'message.html', {
+                    'title': 'Neizdevās aktivizēt kontu!',
+                    'text': 'Tika veikts neveiksmīgs mēģinājums nosūtīt aktivizēšanas saiti uz ' + user.email + ' . Lūdzu pārbaudiet, vai esat pareizi ievadījuši e-pastu!',
+                })
     else:
         form = RegisterUserForm()
 
--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,14 @@ DATABASES = {
     #'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': 'mydatabase',}
 
 }
+
+#Emailing settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'mail.virtualchurch@gmail.com'
+EMAIL_HOST_USER = 'mail.virtualchurch@gmail.com'
+EMAIL_HOST_PASSWORD = 'aohuopziqstrlcqp'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+PASSWORD_RESET_TIMEOUT=14400
